d3957b1c91a1af2b74fe17664782e0892b4f5706
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-05 12:53:26 +0300

    updated: create options menu

--- app/src/main/java/com/ismailmesutmujde/kotlinartbook/view/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinartbook/view/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.ismailmesutmujde.kotlinartbook.view
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import com.ismailmesutmujde.kotlinartbook.R
 import com.ismailmesutmujde.kotlinartbook.databinding.ActivityMainBinding
 
 
@@ -14,6 +18,25 @@ class MainActivity : AppCompatActivity() {
         bindingMainActivity = ActivityMainBinding.inflate(layoutInflater)
         val view = bindingMainActivity.root
         setContentView(view)
-
     }
+
+    // onCreateOptionsMenu : options menüyü, Main Activity'e bağlama işlemi yapılır.
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        // inflater
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.art_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    // onOptionsItemSelected : options menüye tıklanırsa hangi işlemin yapılacağı burada yapılır.
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.add_art_item) {
+            val intent = Intent(this@MainActivity, ArtActivity::class.java)
+            startActivity(intent)
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
 }
